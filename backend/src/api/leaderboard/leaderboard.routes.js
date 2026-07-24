56fcbc03a46db16895b9f58abdbea979a6976478
vanefRiwi <125419162+vanefRiwi@users.noreply.github.com>
2026-07-24 00:18:29 -0500

    Delete backend directory

--- backend/src/api/leaderboard/leaderboard.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getLeaderboard } from "./leaderboard.controllers.js";
-import { verifyJWT } from "../../middlewares/auth.middleware.js";
-
-const router = Router();
-
-router.get("/courses/:courseId/leaderboard", verifyJWT, getLeaderboard);
-
-export default router;
--- backend/src/api/leaderboard/leaderboard.services.js
+++
@@ -1,7 +0,0 @@
-import { leaderboardRepository } from "./leaderboard.repository.js";
-
-export const leaderboardServices = {
-  getForCourse: async (courseId) => {
-    return leaderboardRepository.findForCourse(courseId);
-  },
-};
