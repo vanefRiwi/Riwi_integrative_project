6d92ab5d9695fc91f2b1cc7e5292cdc6cf79c875
vanefRiwi <125419162+vanefRiwi@users.noreply.github.com>
2026-07-25 23:15:15 -0500

    backend directory changed comments to english

--- backend/src/api/grade/grade.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getCourseStudents } from "./grade.controllers.js";
-import { verifyJWT, authorizeRoles } from "../../middlewares/auth.middleware.js";
-
-const router = Router();
-
-router.get("/courses/:courseId/students", verifyJWT, authorizeRoles("tutor"), getCourseStudents);
-
-export default router;
--- backend/src/utils/errorHandler.js
+++
@@ -1,8 +0,0 @@
-// Middleware central de errores (va al final en index.js)
-export const errorHandler = (err, req, res, next) => {
-  console.error("❌", err.message);
-  res.status(err.status || 500).json({
-    ok: false,
-    message: err.message || "Internal Server Error",
-  });
-};
